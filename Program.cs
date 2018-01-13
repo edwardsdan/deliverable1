13142c8677b5a1793bfa1dde481777f0b493b8fb
thelionorion <35340235+thelionorion@users.noreply.github.com>
2018-01-13 12:10:47 -0500

    Cleaned and reworked whole program, unfinished

--- Program.cs
+++ Program.cs
@@ -22,93 +22,42 @@ namespace deliverable1
             return y;
         }
        
-            /* ignore >>>>>>>>>>>
-             * int b = y.Length;
-
-        int a = x.Length;
-            if(a == b)
-            {
-                return (x, y);
-            }
-            else
-            {
-                Console.WriteLine("Sorry! Try again!");
-                x = "";
-                y = "a";
-                Prompt(x, y);
-                return (x, y);
-            }*/
+           
  // Prompt in 2 methods for two separate strings. Compare length with 3rd method?
 
         static void Main(string[] args)
         {
             string firstInt = Prompt1();
             string secondInt = Prompt2();
-            
+
             if (firstInt.Length == secondInt.Length)
             {
-                int[] int1 = new int[firstInt.Length];
-                int[] int2 = new int[secondInt.Length];
-                for (int c = 0; c <= firstInt.Length; c++)
+                char[] One = firstInt.ToCharArray();
+                char[] Two = secondInt.ToCharArray();
+
+                int[] A = One.Select(a => a - '0').ToArray(); // figure out what 'a' represents ***************
+                int[] B = Two.Select(a => a - '0').ToArray(); // pretty sure this uses Linq, learn more
+                int[] C = new int[A.Length];
+                
+                for (int i = 0; i < C.Length; i++)
                 {
-                    int1[c] = Convert.ToInt32(firstInt[c]);
-                    int2[c] = Convert.ToInt32(secondInt[c]);
+                    C[i] = A[i] + B[i];
+                    if (C[i] == C[0])
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
                 }
             }
             else
             {
                 Console.WriteLine("Sorry! Try again!");
-                /* recursive call method to compare string.length*/
+                Main(args);
+                // recursive call method to compare string.length
             }
-            /*int[] p = new int[firstNumber.Length]; // reminder!
-            int[] q = new int[secondNumber.Length];
-            for (int i = 0; i < firstNumber.Length; i++)
-            {
-                p[i] = firstNumber[i] - '0';
-                q[i] = secondNumber[i] - '0';
-                Console.WriteLine(p[i] + " " + q[i]);
-            }*/
         }
-
-
-
-
-        // ignore the rest of this?
-
-            /*if (int.TryParse(firstNumber, out int number1) && int.TryParse(secondNumber, out int number2))
-            {
-                number1 = Convert.ToInt32(firstNumber);
-                number2 = Convert.ToInt32(secondNumber);
-                Console.WriteLine(Test(number1, number2));
-            }*/
-
-
-            /*public static string Test(int x, int y)
-        {
-            string stringX = x.ToString();
-            string stringY = y.ToString();
-            int[] NumbersX = new int[stringX.Length];
-            int[] NumbersY = new int[stringY.Length];
-            int[] Adding = { 0 };
-            string TorF = "";
-
-            for (int i = -1; i < NumbersX.Length; i++)
-            {
-                NumbersX[i+1] = Convert.ToInt32(stringX[i+1]); 
-                NumbersY[i+1] = Convert.ToInt32(stringX[i+1]);
-                Adding[i+1] += NumbersX[i+1] + NumbersY[i+1];
-                if (Adding[i+1] != Adding[i])
-                {
-                    TorF = "false";
-                }
-                else
-                {
-                    TorF = "true";
-                }
-
-            }
-            return TorF;
-
-        }*/
     }
 }
